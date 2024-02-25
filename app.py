e640fbfbe6914bf789d448942d5d6406ea1fd667
snek <felo.markosyan@gmail.com>
2024-02-25 18:29:20 +0400

    update to LCM

--- app.py
+++ app.py
@@ -1,59 +1,61 @@
 import gradio as gr
-from diffusers import EulerAncestralDiscreteScheduler, AutoencoderKL, StableDiffusionXLPipeline
+from diffusers import UNet2DConditionModel, AutoPipelineForImage2Image, LCMScheduler
 import torch
 
+# Load the LCM model
+unet = UNet2DConditionModel.from_pretrained(
+    "latent-consistency/lcm-sdxl",
+    torch_dtype=torch.float16,
+    variant="fp16",
+)
+
+pipe = AutoPipelineForImage2Image.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0",
+    unet=unet,
+    torch_dtype=torch.float16,
+    variant="fp16",
+)
+pipe.scheduler = LCMScheduler.from_config(pipe.scheduler.config)
+
+# Check for GPU and move model appropriately
+# if torch.cuda.is_available():
+pipe.to("cuda")
+# else:
+#     pipe.to("mps")  # For Apple M1/M2 chips
+
 # Function to generate an image
-def generate_image(prompt, negative_prompt, cfg_scale, num_inference_steps, height, width):
-    if torch.cuda.is_available():  # Check for CUDA GPU
-        with torch.autocast("cuda"):
-            image = pipe(
-                prompt,
-                negative_prompt=negative_prompt,
-                guidance_scale=cfg_scale,
-                num_inference_steps=num_inference_steps,
-                height=height,
-                width=width
-            ).images[0]
-    else:  # Use MPS by default
+def generate_image(prompt, init_image):
+    # if torch.cuda.is_available():
+    with torch.autocast("cuda"):
         image = pipe(
-            prompt,
-            negative_prompt=negative_prompt,
-            guidance_scale=cfg_scale,
-            num_inference_steps=num_inference_steps,
-            height=height,
-            width=width
+            prompt = prompt,
+            image = init_image,
+            guidance_scale=7,
+            num_inference_steps=4,
+            strength = 0.4,
+            height=1024,
+            width=1024
         ).images[0]
+    # else:
+    #     image = pipe(
+    #         prompt = prompt,
+    #         image = init_image,
+    #         guidance_scale=7,
+    #         num_inference_steps=4,
+    #         strength = 0.4,
+    #         height=1024,
+    #         width=1024
+    #     ).images[0]
     return image
 
-vae = AutoencoderKL.from_pretrained(
-    "madebyollin/sdxl-vae-fp16-fix",  
-    torch_dtype=torch.float16
-)
-pipe = StableDiffusionXLPipeline.from_single_file(
-    "./models/animagine-xl-3.0.safetensors",
-    vae=vae,
-    torch_dtype=torch.float16,
-    use_safetensors=True,
-)
-pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
-
-# Determine device for optimization
-if torch.cuda.is_available():
-    pipe.to("cuda")
-else:
-    pipe.to("mps")  # For Apple M1/M2 chips
-
-# Gradio interface - Set default resolution here
+# Gradio interface (same as before)
 demo = gr.Interface(
     fn=generate_image,
     inputs=[
-        gr.Textbox(lines=1, placeholder="Enter image description"),
-        gr.Textbox(lines=1, placeholder="Enter negative prompt"),
-        gr.Slider(0, 10, value=7, step=0.5, label="CFG Scale"),
-        gr.Slider(0, 50, value=28, step=1, label="Inference Steps"),
-        gr.Number(value=1216, label="Height"), 
-        gr.Number(value=832, label="Width")  
+        gr.Textbox(lines=1, placeholder="Describe your image"),
+        gr.Image()
     ],
     outputs="image"
 )
+
 demo.launch()
